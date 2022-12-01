62ac19f538a7742ed3e719fb07b154571e597dc3
Ozan MUDUL <mudulozan@gmail.com>
2022-12-01 22:46:37 +0200

    Initial Commit

---
+++ FlagViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlagViewer
+//
+//  Created by Ozan Mudul on 1.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
