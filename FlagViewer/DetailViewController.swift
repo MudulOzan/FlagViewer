56174716891a948219a34b280e607510a2fe9e24
Ozan MUDUL <mudulozan@gmail.com>
2022-12-06 21:44:15 +0200

    Added assets and prepared initial view with navigation controller

---
+++ FlagViewer/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  FlagViewer
+//
+//  Created by Ozan Mudul on 6.12.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var selectedImage: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
