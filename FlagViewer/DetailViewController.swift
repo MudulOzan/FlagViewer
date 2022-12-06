64b61bed39616c4ef67e66f283ab0dd7c64e2f2d
Ozan MUDUL <mudulozan@gmail.com>
2022-12-06 22:16:15 +0200

    DetailViewController

--- FlagViewer/DetailViewController.swift
+++ FlagViewer/DetailViewController.swift
@@ -8,15 +8,28 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     var selectedImage: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        let flagTitle = selectedImage?.split(separator: "@")[0].uppercased()
+        title = flagTitle
+        navigationItem.largeTitleDisplayMode = .never
         
-
-        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
     /*
     // MARK: - Navigation
 
